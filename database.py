db148d07e461ad0f94d67f4e7aa7b8ebe484dbb2
Sowmiya E <sowmiyaeraivan@gmail.com>
2026-09-20 15:43:53 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,29 +1,30 @@
-import sqlite3
+import os
+import psycopg2
 
-connection = sqlite3.connect("study_material.db")
-
+connection = psycopg2.connect(os.environ["DATABASE_URL"])
 cursor = connection.cursor()
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id SERIAL PRIMARY KEY,
     username TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL
 )
 """)
+
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS notes (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id SERIAL PRIMARY KEY,
     subject TEXT NOT NULL,
-    note_type NOT NULL,
+    note_type TEXT NOT NULL,
     title TEXT NOT NULL,
     content TEXT NOT NULL
 )
 """)
 
-
 connection.commit()
+cursor.close()
 connection.close()
 
-print("Database and users table created successfully!")
+print("PostgreSQL database and tables created successfully!")
